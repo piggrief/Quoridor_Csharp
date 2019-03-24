a327132654c05c268c98e2c077ec99d2cd8c6bad
piggrief <zhuzhibeishang@126.com>
2019-03-24 23:53:00 +0800

    First

---
+++ Quoridor_With_C/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Quoridor_With_C
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ChessBoardPB = new CCWin.SkinControl.SkinPictureBox();
+            this.PlaceBoardBTN = new CCWin.SkinControl.SkinButton();
+            this.MoveBTN = new CCWin.SkinControl.SkinButton();
+            this.TestTB = new CCWin.SkinControl.SkinTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ChessBoardPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChessBoardPB
+            // 
+            this.ChessBoardPB.BackColor = System.Drawing.Color.Transparent;
+            this.ChessBoardPB.Location = new System.Drawing.Point(1, 42);
+            this.ChessBoardPB.Name = "ChessBoardPB";
+            this.ChessBoardPB.Size = new System.Drawing.Size(630, 630);
+            this.ChessBoardPB.TabIndex = 0;
+            this.ChessBoardPB.TabStop = false;
+            this.ChessBoardPB.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ChessBoardPB_MouseClick);
+            // 
+            // PlaceBoardBTN
+            // 
+            this.PlaceBoardBTN.BackColor = System.Drawing.Color.Transparent;
+            this.PlaceBoardBTN.BaseColor = System.Drawing.Color.AliceBlue;
+            this.PlaceBoardBTN.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.PlaceBoardBTN.DownBack = null;
+            this.PlaceBoardBTN.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.PlaceBoardBTN.Location = new System.Drawing.Point(898, 119);
+            this.PlaceBoardBTN.MouseBack = null;
+            this.PlaceBoardBTN.Name = "PlaceBoardBTN";
+            this.PlaceBoardBTN.NormlBack = null;
+            this.PlaceBoardBTN.Size = new System.Drawing.Size(194, 54);
+            this.PlaceBoardBTN.TabIndex = 1;
+            this.PlaceBoardBTN.Text = "放置挡板";
+            this.PlaceBoardBTN.UseVisualStyleBackColor = false;
+            this.PlaceBoardBTN.Click += new System.EventHandler(this.PlaceBoardBTN_Click);
+            // 
+            // MoveBTN
+            // 
+            this.MoveBTN.BackColor = System.Drawing.Color.Transparent;
+            this.MoveBTN.BaseColor = System.Drawing.Color.AliceBlue;
+            this.MoveBTN.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.MoveBTN.DownBack = null;
+            this.MoveBTN.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MoveBTN.Location = new System.Drawing.Point(898, 227);
+            this.MoveBTN.MouseBack = null;
+            this.MoveBTN.Name = "MoveBTN";
+            this.MoveBTN.NormlBack = null;
+            this.MoveBTN.Size = new System.Drawing.Size(194, 54);
+            this.MoveBTN.TabIndex = 2;
+            this.MoveBTN.Text = "移动棋子";
+            this.MoveBTN.UseVisualStyleBackColor = false;
+            this.MoveBTN.Click += new System.EventHandler(this.MoveBTN_Click);
+            // 
+            // TestTB
+            // 
+            this.TestTB.BackColor = System.Drawing.Color.Transparent;
+            this.TestTB.DownBack = null;
+            this.TestTB.Icon = null;
+            this.TestTB.IconIsButton = false;
+            this.TestTB.IconMouseState = CCWin.SkinClass.ControlState.Normal;
+            this.TestTB.IsPasswordChat = '\0';
+            this.TestTB.IsSystemPasswordChar = false;
+            this.TestTB.Lines = new string[0];
+            this.TestTB.Location = new System.Drawing.Point(874, 446);
+            this.TestTB.Margin = new System.Windows.Forms.Padding(0);
+            this.TestTB.MaxLength = 32767;
+            this.TestTB.MinimumSize = new System.Drawing.Size(28, 28);
+            this.TestTB.MouseBack = null;
+            this.TestTB.MouseState = CCWin.SkinClass.ControlState.Normal;
+            this.TestTB.Multiline = true;
+            this.TestTB.Name = "TestTB";
+            this.TestTB.NormlBack = null;
+            this.TestTB.Padding = new System.Windows.Forms.Padding(5);
+            this.TestTB.ReadOnly = false;
+            this.TestTB.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TestTB.Size = new System.Drawing.Size(250, 209);
+            // 
+            // 
+            // 
+            this.TestTB.SkinTxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TestTB.SkinTxt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TestTB.SkinTxt.Font = new System.Drawing.Font("微软雅黑", 9.75F);
+            this.TestTB.SkinTxt.Location = new System.Drawing.Point(5, 5);
+            this.TestTB.SkinTxt.Multiline = true;
+            this.TestTB.SkinTxt.Name = "BaseText";
+            this.TestTB.SkinTxt.Size = new System.Drawing.Size(240, 199);
+            this.TestTB.SkinTxt.TabIndex = 0;
+            this.TestTB.SkinTxt.WaterColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            this.TestTB.SkinTxt.WaterText = "";
+            this.TestTB.TabIndex = 3;
+            this.TestTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.TestTB.WaterColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            this.TestTB.WaterText = "";
+            this.TestTB.WordWrap = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 800);
+            this.Controls.Add(this.TestTB);
+            this.Controls.Add(this.MoveBTN);
+            this.Controls.Add(this.PlaceBoardBTN);
+            this.Controls.Add(this.ChessBoardPB);
+            this.Name = "Form1";
+            this.Radius = 35;
+            this.RoundStyle = CCWin.SkinClass.RoundStyle.All;
+            this.Text = "步步为营游戏仿真环境";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.ChessBoardPB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public CCWin.SkinControl.SkinPictureBox ChessBoardPB;
+        private CCWin.SkinControl.SkinButton PlaceBoardBTN;
+        private CCWin.SkinControl.SkinButton MoveBTN;
+        private CCWin.SkinControl.SkinTextBox TestTB;
+    }
+}
+
---
+++ Quoridor_With_C/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CCWin;
+using System.Runtime.InteropServices;  
+
+namespace Quoridor_With_C
+{
+    public partial class Form1 : Skin_VS
+    {
+        [DllImport("kernel32.dll")]
+        public static extern Boolean AllocConsole();
+        [DllImport("kernel32.dll")]
+        public static extern Boolean FreeConsole(); 
+
+        Graphics Gr;
+        Bitmap bmp = new Bitmap(1000, 900);
+
+        enum EnumNowPlayer
+        {
+            Player1,
+            Player2
+        }
+        EnumNowPlayer NowPlayer = EnumNowPlayer.Player1;
+        public enum NowAction
+        {
+            Action_PlaceVerticalBoard,
+            Action_PlaceHorizontalBoard,
+            Action_Move,
+            Action_Wait
+        }
+
+        NowAction P1NowAction = NowAction.Action_Wait;
+        NowAction P2NowAction = NowAction.Action_Wait;
+
+        public class _FormDraw
+        {
+            public static float CB_LineWidth = 0;
+            public static int CB_size_width = 0;
+            public static int CB_size_height = 0;
+
+            /// <summary>
+            /// 画棋盘框线函数
+            /// </summary>
+            /// <param name="Gr">绘画类</param>
+            /// <param name="size_width">棋盘宽度</param>
+            /// <param name="size_height">棋盘高度</param>
+            /// <param name="LineColor">框线颜色</param>
+            /// <param name="LineWidth">框线宽度</param>
+            public void DrawChessBoard(Graphics Gr, int size_width, int size_height, Color LineColor, float LineWidth)
+            {
+                Pen pen = new Pen(LineColor, LineWidth);//定义画笔，里面的参数为画笔的颜色
+
+                for (int i = 0; i < 8; i++)//列线
+                {
+                    Gr.DrawLine(pen, size_width / 8 * i + LineWidth / 2, 0 + LineWidth / 2, size_width / 8 * i + LineWidth / 2, size_width /8 *7 + LineWidth / 2);
+                    Gr.DrawLine(pen, 0 + LineWidth / 2, size_height / 8 * i + LineWidth / 2, size_height / 8 * 7 + LineWidth / 2, size_height / 8 * i + LineWidth / 2);                  
+                }
+
+                CB_LineWidth = LineWidth;
+                CB_size_width = size_width;
+                CB_size_height = size_height;
+            }
+            /// <summary>
+            /// 画挡板
+            /// </summary>
+            /// <param name="Gr">绘画类</param>
+            /// <param name="NA">当前动作状态</param>
+            /// <param name="row">第row行挡板</param>
+            /// <param name="col">第col列挡板</param>
+            /// <param name="BoardColor">挡板颜色</param>
+            /// <param name="BoardWidth">挡板宽度，最好和棋盘框长度一样</param>
+            public void DrawBoard(Graphics Gr, NowAction NA, int row, int col, Color BoardColor, int BoardWidth)
+            {
+                int BlockWidth = _FormDraw.CB_size_width / 8;
+                if (NA == NowAction.Action_PlaceVerticalBoard)
+                {
+                    Pen pen = new Pen(BoardColor, BoardWidth);//定义画笔，里面的参数为画笔的颜色
+
+                    Gr.DrawLine(pen, col * BlockWidth + BoardWidth / 2, row * BlockWidth + BoardWidth, col * BlockWidth + BoardWidth / 2, (row + 1) * BlockWidth);
+                }
+                else if(NA == NowAction.Action_PlaceHorizontalBoard)
+                {
+                    Pen pen = new Pen(BoardColor, BoardWidth);//定义画笔，里面的参数为画笔的颜色
+
+                    Gr.DrawLine(pen, col * BlockWidth + BoardWidth, row * BlockWidth + BoardWidth / 2, (col + 1) * BlockWidth, row * BlockWidth + BoardWidth / 2);                
+                }
+            }
+            /// <summary>
+            /// 画棋子圆
+            /// </summary>
+            /// <param name="Gr">绘画类</param>
+            /// <param name="row">第row行棋格</param>
+            /// <param name="col">第col行棋格</param>
+            /// <param name="ChessColor">棋子颜色</param>
+            /// <param name="LineWidth">棋盘线宽</param>
+            public void DrawChess(Graphics Gr, int row, int col, Color ChessColor, int LineWidth)
+            {
+                int BlockWidth = _FormDraw.CB_size_width / 8;
+                int size_Chess = Convert.ToInt16(BlockWidth * 0.6)-2;
+                Gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                Brush bush = new SolidBrush(ChessColor);//填充的颜色
+
+                Gr.FillEllipse(bush, col* BlockWidth + size_Chess / 2 + 2, row * BlockWidth + size_Chess / 2 + 2, size_Chess, size_Chess);//画填充椭圆的方法，x坐标、y坐标、宽、高，如果是100，则半径为50 
+            }
+            /// <summary>
+            /// 清除棋子圆
+            /// </summary>
+            /// <param name="Gr">绘画类</param>
+            /// <param name="row">第row行棋格</param>
+            /// <param name="col">第col行棋格</param>
+            /// <param name="ClearColor">清除颜色</param>
+            /// <param name="LineWidth">棋盘线宽</param>
+            public void ClearChess(Graphics Gr, int row, int col, Color ClearColor, int LineWidth)
+            {
+                int BlockWidth = _FormDraw.CB_size_width / 8;
+                double size_Chess = Convert.ToDouble(BlockWidth) * 0.6;
+                Gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                Brush bush = new SolidBrush(ClearColor);//填充的颜色
+                int x = Convert.ToInt16(col * BlockWidth + size_Chess / 2);
+                int y = Convert.ToInt16(row * BlockWidth + size_Chess / 2);
+
+                Gr.FillEllipse(bush, x, y, Convert.ToInt16(size_Chess), Convert.ToInt16(size_Chess));//画填充椭圆的方法，x坐标、y坐标、宽、高，如果是100，则半径为50 
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            AllocConsole();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Gr = Graphics.FromImage(bmp);
+            _FormDraw FD = new _FormDraw();
+
+            Gr.Clear(Color.FromArgb(42, 58, 86));
+            FD.DrawChessBoard(Gr, 630, 630, Color.FromArgb(163,159,147), 15);
+            ChessBoardPB.Size = new Size(630, 630);
+
+            ChessBoardPB.BackgroundImage = bmp;
+        }
+
+
+        private void PlaceBoardBTN_Click(object sender, EventArgs e)
+        {
+            if (NowPlayer == EnumNowPlayer.Player1)
+            {
+                P1NowAction = NowAction.Action_PlaceHorizontalBoard;
+            }
+        }
+
+        private void MoveBTN_Click(object sender, EventArgs e)
+        {
+            if (NowPlayer == EnumNowPlayer.Player1)
+            {
+                P1NowAction = NowAction.Action_Move;
+            }
+        }
+        public System.Drawing.Point TP = new System.Drawing.Point();
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+    
+        }
+        bool flag_clear = true;
+        private void ChessBoardPB_MouseClick(object sender, MouseEventArgs e)
+        {
+            TP = e.Location;
+            //Console.WriteLine("MouseClick!");
+            if (NowPlayer == EnumNowPlayer.Player1)
+            {
+                if (P1NowAction == NowAction.Action_PlaceVerticalBoard)
+                {
+                    int col = 0, row = 0;//0~7行列
+                    int BlockWidth = _FormDraw.CB_size_width / 8;
+                    col = (TP.X + BlockWidth/2) / BlockWidth;
+                    row = (TP.Y) / BlockWidth;
+                    //Console.WriteLine("挡板位置：" + col.ToString() +","+ row.ToString());
+                    _FormDraw FD = new _FormDraw();
+                    FD.DrawBoard(Gr, NowAction.Action_PlaceVerticalBoard, row, col, Color.Red, 15);
+                    //ChessBoardPB.Size = new Size(630, 630);
+                    //ChessBoardPB.BackgroundImage = bmp;
+                    ChessBoardPB.Refresh();
+                }
+                else if (P1NowAction == NowAction.Action_PlaceHorizontalBoard)
+                {
+                    int col = 0, row = 0;//0~7行列
+                    int BlockWidth = _FormDraw.CB_size_width / 8;
+                    col = (TP.X) / BlockWidth;
+                    row = (TP.Y + BlockWidth/2) / BlockWidth;
+                    //Console.WriteLine("挡板位置：" + col.ToString() +","+ row.ToString()); 
+                    _FormDraw FD = new _FormDraw();
+                    FD.DrawBoard(Gr, NowAction.Action_PlaceHorizontalBoard, row, col, Color.Red, 15);
+                    ChessBoardPB.Refresh();
+
+                }
+                else if (P1NowAction == NowAction.Action_Move)
+                {
+                    int col = 0, row = 0;//0~7行列
+                    int BlockWidth = _FormDraw.CB_size_width / 8;
+                    col = (TP.X) / BlockWidth;
+                    row = (TP.Y) / BlockWidth;
+                    Console.WriteLine("挡板位置：" + col.ToString() +","+ row.ToString()); 
+                    _FormDraw FD = new _FormDraw();
+                    //FD.DrawBoard(Gr, NowAction.Action_PlaceHorizontalBoard, row, col, Color.Red, 15);
+                    flag_clear = !flag_clear;
+
+                    if (flag_clear)
+                        FD.ClearChess(Gr, row, col, Color.FromArgb(42, 58, 86), 15);
+                    else
+                        FD.DrawChess(Gr, row, col, Color.Green,15);
+                    ChessBoardPB.Refresh(); 
+                }
+            }
+        }
+
+    }
+}
